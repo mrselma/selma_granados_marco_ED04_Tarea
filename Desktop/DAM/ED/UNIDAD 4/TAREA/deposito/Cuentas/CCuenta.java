89e9f36cb2a87f40817824fd1299c154636686da
mrsel <mrsel@LAPTOP-ENVN135G>
2022-02-05 18:48:28 +0100

    dsadsadsa

---
+++ Desktop/DAM/ED/UNIDAD 4/TAREA/deposito/Cuentas/CCuenta.java
@@ -0,0 +1,84 @@
+package Cuentas;
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInters;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        setNombre(nom);
+        setCuenta(cue);
+        saldo=sal;
+    }
+
+    public double estado()
+    {
+        return saldo;
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        saldo = saldo - cantidad;
+    }
+    public void operativaCuenta(float cantaretirar, float cantaingresar) {
+               try {
+				retirar(cantaretirar);
+			} catch (Exception e) {
+				// xsxx TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+          
+               System.out.println("Ingreso en cuenta");
+               System.out.println("Ingreso en cuenta22");
+               try {
+				ingresar(cantaingresar);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        
+          
+    }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getTipoInters() {
+		return tipoInters;
+	}
+
+	public void setTipoInters(double tipoInters) {
+		this.tipoInters = tipoInters;
+	}
+}
