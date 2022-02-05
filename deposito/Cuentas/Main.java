e9891e8ad035178b967873ebb249ebbeaaadd859
mrselma <55976519+mrselma@users.noreply.github.com>
2022-02-05 17:36:33 +0100

    Versión del proyecto deposito
    Proyecto deposito realizado con las modificaciones solicitadas en los ejercicios de la tarea de la unidad 4 de ED.

---
+++ deposito/Cuentas/Main.java
@@ -0,0 +1,19 @@
+package Cuentas;
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta Cuenta1;
+        double saldoActual;
+
+        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = Cuenta1.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+
+        try {
+           Cuenta1.operativaCuenta(2300,695);
+        } catch (Exception e) {
+            System.out.print("Fallo en operativa");
+        }
+       
+    }
+}
